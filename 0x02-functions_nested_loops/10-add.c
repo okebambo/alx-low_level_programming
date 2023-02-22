84c79023c4cd5ccf3e723c4abf10209fc7c7330b
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-22 04:10:26 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add- additon
+ *
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
