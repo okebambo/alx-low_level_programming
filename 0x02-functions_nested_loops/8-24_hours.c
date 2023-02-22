5e157ff11134dd9ae093b3cb269a3d2c0bd9e927
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-22 02:27:21 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,23 +16,11 @@ void jack_bauer(void)
 	{
 		for (j = 0; j <= 59; j++)
 		{
-			if (i < 9)
-			{
-				_putchar ('0' + i);
-			}
-			else
-			{
-				_putchar (i);
-			}
+			_putchar ((i / 10) + '0');
+			_putchar ((i % 10) + '0');
 			_putchar (':');
-			if (j < 10)
-			{
-				_putchar ('0' + j);
-			}
-			else
-			{
-				_putchar (j);
-			}
+			_putchar ((j / 10) + '0');
+			_putchar ((j % 10) + '0');
 			_putchar ('\n');
 		}
 	}
