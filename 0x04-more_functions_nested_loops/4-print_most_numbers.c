60027484525fbf114aa47f81dc1181bfa1af5389
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-24 14:12:13 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - 4-print_most_numbers.c
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		if (a == 2 || a == 4)
+		{
+			a++;
+		}
+		else
+		{
+			_putchar(a);
+		}
+}
