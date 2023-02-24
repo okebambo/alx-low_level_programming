36105aecaea26c1658642593929fc3b7b881b932
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-24 12:31:52 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - 3-print_numbers.c
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
