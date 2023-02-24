e98b1543e3545da02c47a68f165b466fb3e286a2
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-24 12:17:49 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - 2-mul.c
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
