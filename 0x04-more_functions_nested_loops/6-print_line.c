7251bdcf28023c9cf9e46eb60d5840758fe3836d
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-24 14:44:59 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - 6-print_line.c
+ *
+ * @n: lime input
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
