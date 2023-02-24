a8a6bf5e6edc062cf467131423f4b43475b3e6a2
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-24 07:40:14 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - 100-prime_factor.c
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ project task on 0x04-more_functions_nested_loops
