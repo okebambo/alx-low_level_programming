589d50eaa0d7b776769ebf54e8c5bde64abba23d
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-24 13:27:27 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - more_numbers.c
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, a;
+
+	while (i <= 10)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar('a');
+			_putchar('\n');
+		}
+	}
+	_putchar('i');
+	i++;
+}
