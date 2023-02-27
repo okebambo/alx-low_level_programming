613cbe805465ff9f9e7912827b3b75a51dd9121c
Okebambo Seun Joseph <seunokebambo@gmail.com>
2023-02-27 04:48:24 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset to 98.
+ * @n: input
+ * Return: 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * swap_int - change of numbers
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
